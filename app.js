6c9c223b46458abf15d038b3f57e537305a846ab
YugaF <105905766+YugaF@users.noreply.github.com>
2022-06-04 09:30:13 +0700

    Update app.js

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ app.get('/', function (req, res) {
   });
 })
 
+app.post('/post', function (req, res) {
+  res.json({
+    number: 1,
+    nama: 'Kurniawan',
+    npm: '2042000'
+  });
+})
+
 app.set('port', (process.env.PORT || 8080));
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
